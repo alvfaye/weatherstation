59fddb32c13621bd289c896022b6f3bf29e3a95e
alvfaye <alvfaye@gmail.com>
2022-09-02 02:05:48 +0800

    format error msg

--- api/src/services/weather/weather.js
+++ api/src/services/weather/weather.js
@@ -1,10 +1,19 @@
-import { fetch } from 'cross-undici-fetch'
 const API_KEY = "b76dcd7e89746c4fd4baa3c11b0b0553"
+
+import { fetch } from 'cross-undici-fetch'
+import { UserInputError } from '@redwoodjs/graphql-server'
+
 export const getWeather = async ({ zip }) => {
   const response = await fetch(
     `http://api.openweathermap.org/data/2.5/weather?zip=${zip},US&appid=${API_KEY}`
   )
   const json = await response.json()
+
+  if (json.cod === '404') {
+    throw new UserInputError(
+      `${zip} isn't a valid US zip code, please try again`
+    )
+  }
 
   return {
     zip,
