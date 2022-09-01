f8025c0bde93e6dc2318b0c7bd0f5ff77a3b6085
alvfaye <alvfaye@gmail.com>
2022-09-02 01:53:36 +0800

    call undici into the service and fetch

--- api/src/services/weather/weather.js
+++ api/src/services/weather/weather.js
@@ -1,11 +1,16 @@
-export const getWeather = ({ zip }) => {
+import { fetch } from 'cross-undici-fetch'
+const API_KEY = "b76dcd7e89746c4fd4baa3c11b0b0553"
+export const getWeather = async ({ zip }) => {
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?zip=${zip},US&appid=${API_KEY}`
+  )
+  const json = await response.json()
+
   return {
     zip,
-    city: 'City',
-    conditions: 'Hot Lava',
-    temp: 1000,
-    icon: 'https://placekitten.com/100/100',
+    city: json.name,
+    conditions: json.weather[0].main,
+    temp: Math.round(((json.main.temp - 273.15) * 9) / 5 + 32),
+    icon: `http://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png`,
   }
 }
-
-
