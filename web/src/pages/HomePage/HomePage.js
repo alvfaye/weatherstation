f880b24ec0332ed082386be58a671598fbe9af32
alvfaye <alvfaye@gmail.com>
2022-09-01 08:22:20 +0800

    add open weather api key

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,9 +1,14 @@
 import { Form, TextField, Submit } from '@redwoodjs/forms'
 
+// const apikey = process.env.REDWOOD_ENV_OPEN_WEATHER_API_KEY
 const HomePage = () => {
-  const onSubmit = (data) => {
-    console.info(data)
-  }
+const onSubmit = (data) => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?zip=66952,us&appid=b76dcd7e89746c4fd4baa3c11b0b0553`
+  )
+    .then((response) => response.json())
+    .then((json) => console.info(json))
+}
 
   return (
     <Form onSubmit={onSubmit} style={{ fontSize: '2rem' }}>
