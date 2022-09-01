ea8c8598cbaf458c6da828bef5401585248f6e56
alvfaye <alvfaye@gmail.com>
2022-09-02 02:00:23 +0800

    update homepage to keep track of zip

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,23 +1,14 @@
 import { Form, TextField, Submit } from '@redwoodjs/forms'
 import { useState } from 'react'
-// const apikey = process.env.REDWOOD_ENV_OPEN_WEATHER_API_KEY
+import WeatherCell from 'src/components/WeatherCell'
+
 const HomePage = () => {
-  const [weather, setWeather] = useState()
-const onSubmit = (data) => {
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?zip=${data.zip},us&appid=b76dcd7e89746c4fd4baa3c11b0b0553`
-  )
-    .then((response) => response.json())
-    .then((json) => setWeather(json))
-}
+  const [zip, setZip] = useState()
 
-    const temp = () => Math.round(((weather.main.temp - 273.15) * 9) / 5 + 32)
+  const onSubmit = (data) => {
+    setZip(data.zip)
+  }
 
-    const condition = () => weather.weather[0].main
-
-    const icon = () => {
-      return `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
-    }
   return (
     <>
       <Form onSubmit={onSubmit} style={{ fontSize: '2rem' }}>
@@ -29,17 +20,7 @@ const onSubmit = (data) => {
         />
         <Submit>Go</Submit>
       </Form>
-      {weather && (
-        <section>
-          <h1>{weather.name}</h1>
-          <h2>
-            <img src={icon()} style={{ maxWidth: '2rem' }} />
-            <span>
-              {temp()}°F and {condition()}
-            </span>
-          </h2>
-        </section>
-      )}{' '}
+      {zip && <WeatherCell zip={zip} />}
     </>
   )
 }
